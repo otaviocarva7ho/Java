bb0f6191b60179b28404fe50b4e39a71611a2a85
Otávio Carvalho <carvalhom7otavio@gmail.com>
2025-03-05 15:03:03 -0300

    Operadores Aritméticos

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,15 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int soma = 5 + 3; // Resultado: 8
+        int subtracao = 10 - 4; // Resultado: 6
+        int multiplicacao = 6 * 3; // Resultado: 18
+        int divisao = 10 / 2; // Resultado: 5
+        int resto = 10 % 3; // Resultado: 1
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+        System.out.println(resto);
+        
+    }
+}
